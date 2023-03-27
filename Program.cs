623de606fec6fb37fef3f0f7c7193e20fd70442a
shuckster87 <113372474+shuckster87@users.noreply.github.com>
2023-03-26 23:25:33 -0400

    Testing C#
    Testing out input and output of C# using addition

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.WriteLine("Enter a number: ");
+var num = Console.ReadLine();
+Console.WriteLine("The number you entered is " + num);
+
+Console.WriteLine("Enter another number: ");
+var num2 = Console.ReadLine();
+Console.WriteLine("The number you entered is " + num2); 
+
+var add = num + num2;
+Console.WriteLine("addition: " + add);
